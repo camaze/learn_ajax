73d3dab6615581129b6834ab797e437528c0476f
Nian <1046783533@qq.com>
2020-09-28 18:39:26 +0800

    saved temprorily

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const {
-    request
-} = require("http")
-
 myButton.addEventListener('click', (e) => {
-    request = new XMLHttpRequest()
-    request.open('GET')
+    let request = new XMLHttpRequest()
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            console.log('请求响应完成')
+            if (request.status >= 200 && request.status < 300) {
+                console.log('说明请求成功')
+                console.log(request.responseText)
+            } else if (request.status >= 400) {
+                console.log('说明请求失败')
+            }
+        }
+    }
+    request.open('GET', '/xxx') // set request
+    request.send()
 })
