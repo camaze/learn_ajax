83d943a2fbf5fad444b705a57638c59bd55ebcc7
Nian <1046783533@qq.com>
2020-10-03 20:39:12 +0800

    self made jQuery-ajax

--- main.js
+++ main.js
@@ -1,24 +1,66 @@
-myButton.addEventListener('click', (e) => {
+window.jQuery = function (nodeOrSelector) {
+    let nodes = {}
+    nodes.addClass = function () {}
+    nodes.html = function () {}
+    return nodes
+}
+
+window.jQuery.ajax = function (options) {
+    let url
+    if (arguments.length === 1) {
+        url = options.url
+    } else if (arguments.length === 2) {
+        url = arguments[0]
+        options = arguments[1]
+    }
+    let method = options.method
+    let body = options.body
+    let successFn = options.successFn
+    let failFn = options.failFn
+    let headers = options.headers
+
     let request = new XMLHttpRequest()
-    request.open('GET', 'http://jack.com:8888/xxx') // set request
-    request.send()
+    request.open(method, url) // set request
+    for (let key in headers) {
+        let value = headers[key]
+        request.setRequestHeader(key, value)
+    }
     request.onreadystatechange = () => {
         if (request.readyState === 4) {
-            console.log('请求响应完成')
-            console.log(request.status)
             if (request.status >= 200 && request.status < 300) {
-                console.log('说明请求成功')
-                console.log(request.responseText)
-                let text = request.responseText
-                // 把符合JSON语法的字符串
-                // 转换成 JS 对应的值
-                let object = window.JSON.parse(text)
-                console.log(typeof object)
-                console.log(object)
+                successFn.call(undefined, request.responseText)
 
             } else if (request.status >= 400) {
                 console.log('说明请求失败')
+                failFn.call(undefined, request)
             }
         }
     }
+    request.send(body)
+}
+
+window.$ = window.jQuery
+
+function f1(responseText) {}
+
+function f2(responseText) {}
+
+
+myButton.addEventListener('click', (e) => {
+
+    window.jQuery.ajax({
+        url: '/xx',
+        method: 'get',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'frank': '18'
+        },
+        successFn: (x) => {
+            console.log(x)
+        },
+        failFn: (x) => {
+            console.log(x)
+            console.log(x.status)
+        }
+    })
 })
