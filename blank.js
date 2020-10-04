37ca98a6498b15a4363d5a645fe6dc75d6f83ae2
Nian <1046783533@qq.com>
2020-10-04 09:44:18 +0800

    before promise

--- blank.js
+++ blank.js
@@ -1,3 +1,17 @@
-var a = [1, 2, 3];
-a.xxx = 'xxx'
+// var a = [1, 2, 3];
+// a.xxx = 'xxx'
+// console.log(a)
+
+a = [1, 2, 3]
+
 console.log(a)
+
+function aaa() {
+    for (let i = 0; i < a.length; i++) {
+        console.log(a[i])
+    }
+}
+
+aaa()
+
+console.log('i=', i)
