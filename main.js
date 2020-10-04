d33f253996346d5da15b5b8cc3e553bfcf1a847c
Nian <1046783533@qq.com>
2020-10-04 23:26:03 +0800

    before self-made ajax promise

--- main.js
+++ main.js
@@ -1,60 +1,12 @@
-window.jQuery = function (nodeOrSelector) {
-    let nodes = {}
-    nodes.addClass = function () {}
-    nodes.html = function () {}
-    return nodes
-}
-
-window.jQuery.ajax = function ({
-    url,
-    method,
-    body,
-    successFn,
-    failFn,
-    headers
-}) {
-    let request = new XMLHttpRequest()
-    request.open(method, url) // set request
-    for (let key in headers) {
-        let value = headers[key]
-        request.setRequestHeader(key, value)
-    }
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                successFn.call(undefined, request.responseText)
-
-            } else if (request.status >= 400) {
-                console.log('说明请求失败')
-                failFn.call(undefined, request)
-            }
-        }
-    }
-    request.send(body)
-}
-
-window.$ = window.jQuery
-
-function f1(responseText) {}
-
-function f2(responseText) {}
-
-
 myButton.addEventListener('click', (e) => {
-
-    window.jQuery.ajax({
-        url: '/xx',
+    $.ajax({
+        url: '/xxx',
         method: 'get',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'frank': '18'
+    }).then(
+        (responseText) => {
+            console.log(responseText)
         },
-        successFn: (x) => {
-            console.log(x)
-        },
-        failFn: (x) => {
-            console.log(x)
-            console.log(x.status)
-        }
-    })
+        (request) => {
+            console.log(request)
+        })
 })
