477734d83b3bb576a7fcd5ec8ec48f9940cd9c4e
Nian <1046783533@qq.com>
2020-10-04 23:55:24 +0800

    self-made ajax implemented

--- main.js
+++ main.js
@@ -1,12 +1,53 @@
+window.jQuery = function (nodeOrSelector) {
+    let nodes = {}
+    nodes.addClass = function () {}
+    nodes.html = function () {}
+    return nodes
+}
+
+window.jQuery.ajax = function ({
+    url,
+    method,
+    body,
+    headers
+}) {
+    return new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest()
+        request.open(method, url) // set request
+        for (let key in headers) {
+            let value = headers[key]
+            request.setRequestHeader(key, value)
+        }
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    resolve.call(undefined, request.responseText)
+                } else if (request.status >= 400) {
+                    console.log('说明请求失败')
+                    reject.call(undefined, request)
+                }
+            }
+        }
+        request.send(body)
+    })
+}
+window.$ = window.jQuery
+
+
 myButton.addEventListener('click', (e) => {
-    $.ajax({
+    window.jQuery.ajax({
         url: '/xxx',
         method: 'get',
+        headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'frank': '18'
+        }
     }).then(
-        (responseText) => {
-            console.log(responseText)
+        (text) => {
+            console.log(text)
         },
         (request) => {
             console.log(request)
-        })
+        }
+    )
 })
